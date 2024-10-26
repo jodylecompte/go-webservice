29bd548e3a9aaac4181edfd0f10e300ce1fafbe4
Jody LeCompte <jody@jodylecompte.com>
2024-10-26 12:33:18 -0500

    Serving static route

--- controllers/user.go
+++ controllers/user.go
@@ -2,14 +2,23 @@ package controllers
 
 import (
 	"net/http"
+	"regexp"
 )
 
-type userController struct{}
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
 
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("Hello from the user controller"))
 	if err != nil {
 		http.Error(w, "Failed to write response", http.StatusInternalServerError)
 		return
+	}
+}
+
+func newUserController() *userController {
+	return &userController{
+		userIDPattern: regexp.MustCompile("^/users/([0-9]+)"),
 	}
 }
--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/jodylecompte/go-webservice/models"
+	"github.com/jodylecompte/go-webservice/controllers"
+	"net/http"
 )
 
 func main() {
-	u := models.User{
-		ID:        2,
-		FirstName: "Jody",
-		LastName:  "LeCompte",
+	controllers.RegisterControllers()
+	err := http.ListenAndServe(":4500", nil)
+	if err != nil {
+		fmt.Println(err)
 	}
-
-	fmt.Println(u)
 }
