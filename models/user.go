351a2e9ef2ebaf8c85c02d63f349aa3d152c9546
Jody LeCompte <jody@jodylecompte.com>
2024-10-26 12:52:22 -0500

    Course final code

--- models/user.go
+++ models/user.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type User struct {
 	ID        int
 	FirstName string
@@ -7,17 +9,46 @@ type User struct {
 }
 
 var (
-	users  []*User
-	nextID = 1
+	users  = []*User{}
+	nextID = 0
 )
-
-func GetUsers() []*User {
-	return users
-}
 
 func AddUser(u User) (User, error) {
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUsers() []*User {
+	return users
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+func RemoveUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
